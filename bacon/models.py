47015598b0a2443873419dae689fd116751e8b36
Mike Borozdin <nolongemails@gmail.com>
2017-04-02 07:29:38 -0700

    simple models added

--- bacon/models.py
+++ bacon/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Design(models.Model):
+    """
+    dummy class 1
+    """
+    name = models.CharField(max_length=255)
+
+
+class DesignFeatures(models.Model):
+    """
+    dummy class 2
+    """
+    lead_free = models.BooleanField()
+    thickness = models.DecimalField(decimal_places=4, max_digits=10)
+
+    """List of board features"""
+    design = models.OneToOneField(
+        Design,
+        on_delete=models.CASCADE,
+        related_name='features',
+    )
--- bug/urls.py
+++ bug/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('bacon.urls')),
     url(r'^bacon/', include('bacon.urls')),
     url(r'^admin/', admin.site.urls),
 ]
